cb99c946db763df7ed5663bcdd5f7a2708e4f2e7
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-11-25 18:31:08 -0300

    refactor: changing the layout and refactoring the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Tela from './components/Tela'
+import HomePage from './components/HomePage'
 import ResetCss from './style/reset'
 
 function App() {
   return (
     <div>
       <ResetCss/>
-      <Tela></Tela>
+      <HomePage/>
     </div>
   )
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,7 +4,7 @@ import { IoArrowRedo } from "react-icons/io5";
 import { IoArrowUndo } from "react-icons/io5";
 import { BsEraser } from "react-icons/bs";
 
-export default function Tela (){
+export default function HomePage (){
     const [dotsList, setDotsList] = useState([])
     const [removedDots, setRemovedDots] = useState([])
 
@@ -15,17 +15,20 @@ export default function Tela (){
         setRemovedDots([])
     }
 
-    function handleClear(){
+    function handleClear(event){
+        event.stopPropagation()
         setDotsList([])
         setRemovedDots([])
     }
 
-    function handleUndo(){
+    function handleUndo(event){
+        event.stopPropagation()
         setRemovedDots([...removedDots, dotsList.pop()])
         setDotsList(dotsList)
     }
 
-    function handleRedo(){
+    function handleRedo(event){
+        event.stopPropagation()
         setDotsList([...dotsList, removedDots.pop()])
         setRemovedDots(removedDots)
     }
@@ -33,13 +36,13 @@ export default function Tela (){
     return(
         <PageContainer>
             <ScreenContainer onClick={handleClick}>
-                {dotsList.map((dot, index)=> <Dot key={index} style={{left: dot.clientX - 185, top: dot.clientY - 50}}></Dot>)}
-            </ScreenContainer>
+                {dotsList.map((dot, index)=> <Dot key={index} style={{left: dot.clientX - 10, top: dot.clientY - 10}}></Dot>)}
             <ButtonContainer>
-                <Undo onClick={handleUndo} disabled={dotsList.length === 0}> <IoArrowUndo/> Undo</Undo>
-                <Undo onClick={handleRedo} disabled={removedDots.length === 0}> <IoArrowRedo/> Redo</Undo>
-                <Clear onClick={handleClear}> <BsEraser /> Clear</Clear>
+                <button onClick={handleUndo} disabled={dotsList.length === 0}> <IoArrowUndo/> Desfazer</button>
+                <button onClick={handleRedo} disabled={removedDots.length === 0}> <IoArrowRedo/> Refazer</button>
+                <button onClick={handleClear}> <BsEraser /> Limpar</button>
             </ButtonContainer>
+            </ScreenContainer>
         </PageContainer>
     )
 }
@@ -48,7 +51,6 @@ const PageContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
     height: 100dvh;
     background-color: #9165fa;
 `
@@ -56,8 +58,8 @@ const PageContainer = styled.div`
 const ScreenContainer = styled.div`
     background-color: white;
     position: relative;
-    width: 1500px;
-    height: 800px;
+    width: 100%;
+    height: 100%;
     overflow: hidden;
     &:hover{
         cursor: pointer;
@@ -66,6 +68,7 @@ const ScreenContainer = styled.div`
 
 const Dot = styled.div`
     position: absolute;
+    z-index: 0;
     width: 20px;
     height: 20px;
     border-radius: 50%;
@@ -73,11 +76,12 @@ const Dot = styled.div`
 `
 
 const ButtonContainer = styled.div`
-    margin-top: 30px;
+    margin-top: 20px;
     display: flex;
     justify-content: center;
     gap: 20px;
     button{
+        z-index: 1;
         height: 60px;
         width: 120px;
         background-color: #c891fc;
@@ -93,12 +97,4 @@ const ButtonContainer = styled.div`
             filter: brightness(0.9);
         }
     }
-`
-
-const Undo = styled.button`
-    width: 120px;
-`
-
-const Clear = styled.button`
-    width: 120px;
 `
